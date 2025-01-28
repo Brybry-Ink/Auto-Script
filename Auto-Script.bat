7492f58b530e5ae12b6e97cf66502e7e880e959e
Brybry-Ink <bryanzaretti@gmail.com>
2025-01-28 12:45:05 -0500

    Update Auto-Script.bat

--- Auto-Script.bat
+++ Auto-Script.bat
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-    # Function that asks yes or no to install certain programs
+    # function that asks yes or no to install certain programs
     install_program()
     {
+        program_name=$1
 
-
+        # asks user for confirmation
+        read -p "Would you like to install $program_name? (y/n): " choice
 
 
     }
