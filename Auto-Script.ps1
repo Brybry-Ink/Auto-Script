f78c0d62537babdf7f2f5c132948f168278a9bd9
Brybry-Ink <bryanzaretti@gmail.com>
2025-03-06 13:46:12 -0500

    pretty-ify the main menu

--- Auto-Script.ps1
+++ Auto-Script.ps1
@@ -20,6 +20,9 @@ do
     Write-Host "           [1] Windows"
     Write-Host "           [2] Linux"
     Write-Host ""
+    Write-Host "      ______________________________________________________________________      "
+    Write-Host ""
+    Write-Host ""
     $userOSInput = Read-Host "           Choose a menu option using your keyboard [1,2,3...E,X,0] "
 
     switch ($userOSInput) 
