886dceb19a6d280e8a88392b78c578cf5c7885ad
Brybry-Ink <bryanzaretti@gmail.com>
2025-03-25 10:16:02 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,7 @@
+# Get-ExecutionPolicy -List
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+# if running script is giving issues then you need to change the execution policy on your pc using commands above^
+
+# put the auto-script file on your desktop in order to run the run.ps1 script
 cls
-& "C:\Auto-Script.ps1"
+$scriptPath = Join-Path -Path $env:USERPROFILE -ChildPath 'Desktop\Auto-Script.ps1'
